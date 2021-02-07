04a2e09769f93b441af9d11730ce7f99a5e53367
haopeiwei <haopeiwei@vipkid.com.cn>
2021-02-07 15:41:42 +0800

    fabric

--- src/page/util.js
+++ src/page/util.js
@@ -43,4 +43,17 @@ const scaleWH = (imageW, imageH, maxWidth, maxHeight) => {
   }
 };
 
-export { scaleWH, imageList };
+const DEFAULT_CONFIGH = {
+  colorList: ["#b92b27", "#ee9ca7", "#1565C0", "#a8ff78"],
+  color: "#b92b27",
+  pencilSizeList: [1, 2, 3, 4],
+  pencilSize: 1
+  // rotateLeft: true,
+  // rotateRight: true,
+  // zoomIn: true,
+  // zoomOut: true,
+  // prev: true,
+  // next: true
+};
+
+export { scaleWH, imageList, DEFAULT_CONFIGH };
